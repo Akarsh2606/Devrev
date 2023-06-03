7bb2a60b14dd0ef851232a2ebce992792bc185ab
Akarsh2606 <115339906+Akarsh2606@users.noreply.github.com>
2023-06-03 13:28:59 +0530

    commit

---
+++ book.js
@@ -0,0 +1,577 @@
+var data = {
+    "data": [
+        { 
+            SNO: 1,
+            TITLE: "The Catcher in the Rye",
+            AUTHOR: "J.D. Salinger",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1951-07-16"
+          },
+          {
+            SNO: 2,
+            TITLE: "To Kill a Mockingbird",
+            AUTHOR: "Harper Lee",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1960-07-11"
+          },
+          {
+            SNO: 3,
+            TITLE: "Harry Potter and the Sorcerer's Stone",
+            AUTHOR: "J.K. Rowling",
+            SUBJECT: "Fantasy",
+            "PUBLISH DATE": "1997-06-26"
+          },
+          {
+            SNO: 4,
+            TITLE: "The Lord of the Rings",
+            AUTHOR: "J.R.R. Tolkien",
+            SUBJECT: "Fantasy",
+            "PUBLISH DATE": "1954-07-29"
+          },
+          {
+            SNO: 5,
+            TITLE: "1984",
+            AUTHOR: "George Orwell",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1949-06-08"
+          },
+          {
+            SNO: 6,
+            TITLE: "Brave New World",
+            AUTHOR: "Aldous Huxley",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1932-01-01"
+          },
+          {
+            SNO: 7,
+            TITLE: "The Hobbit",
+            AUTHOR: "J.R.R. Tolkien",
+            SUBJECT: "Fantasy",
+            "PUBLISH DATE": "1937-09-21"
+          },
+          {
+            SNO: 8,
+            TITLE: "Pride and Prejudice",
+            AUTHOR: "Jane Austen",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1813-01-28"
+          },
+          {
+            SNO: 9,
+            TITLE: "The Alchemist",
+            AUTHOR: "Paulo Coelho",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1988-01-01"
+          },
+          {
+            SNO: 10,
+            TITLE: "The Da Vinci Code",
+            AUTHOR: "Dan Brown",
+            SUBJECT: "Mystery",
+            "PUBLISH DATE": "2003-03-18"
+          },
+          {
+            SNO: 11,
+            TITLE: "The Chronicles of Narnia",
+            AUTHOR: "C.S. Lewis",
+            SUBJECT: "Fantasy",
+            "PUBLISH DATE": "1950-01-01"
+          },
+          {
+            SNO: 12,
+            TITLE: "The Picture of Dorian Gray",
+            AUTHOR: "Oscar Wilde",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1890-01-01"
+          },
+          {
+            SNO: 13,
+            TITLE: "The Hunger Games",
+            AUTHOR: "Suzanne Collins",
+            SUBJECT: "Science Fiction",
+            "PUBLISH DATE": "2008-09-14"
+          },
+          {
+            SNO: 14,
+            TITLE: "The Book Thief",
+            AUTHOR: "Markus Zusak",
+            SUBJECT: "Historical Fiction",
+            "PUBLISH DATE": "2005-03-14"
+          },
+          {
+            SNO: 15,
+            TITLE: "Moby-Dick",
+            AUTHOR: "Herman Melville",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1851-10-18"
+          },
+          {
+            SNO: 16,
+            TITLE: "The Little Prince",
+            AUTHOR: "Antoine de Saint-Exupéry",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1943-01-01"
+          },
+          {
+            SNO: 17,
+            TITLE: "Gone Girl",
+            AUTHOR: "Gillian Flynn",
+            SUBJECT: "Mystery",
+            "PUBLISH DATE": "2012-06-05"
+          },
+          {
+            SNO: 18,
+            TITLE: "The Kite Runner",
+            AUTHOR: "Khaled Hosseini",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "2003-05-29"
+          },
+          {
+            SNO: 19,
+            TITLE: "The Shining",
+            AUTHOR: "Stephen King",
+            SUBJECT: "Horror",
+            "PUBLISH DATE": "1977-01-28"
+          },
+          {
+            SNO: 20,
+            TITLE: "The Fault in Our Stars",
+            AUTHOR: "John Green",
+            SUBJECT: "Young Adult",
+            "PUBLISH DATE": "2012-01-10"
+          },
+          {
+            SNO: 21,
+            TITLE: "One Hundred Years of Solitude",
+            AUTHOR: "Gabriel García Márquez",
+            SUBJECT: "Magical Realism",
+            "PUBLISH DATE": "1967-01-01"
+          },
+          {
+            SNO: 22,
+            TITLE: "A Game of Thrones",
+            AUTHOR: "George R.R. Martin",
+            SUBJECT: "Fantasy",
+            "PUBLISH DATE": "1996-08-01"
+          },
+          {
+            SNO: 23,
+            TITLE: "The Girl on the Train",
+            AUTHOR: "Paula Hawkins",
+            SUBJECT: "Thriller",
+            "PUBLISH DATE": "2015-01-13"
+          },
+          {
+            SNO: 24,
+            TITLE: "The Count of Monte Cristo",
+            AUTHOR: "Alexandre Dumas",
+            SUBJECT: "Adventure",
+            "PUBLISH DATE": "1844-01-01"
+          },
+          {
+            SNO: 25,
+            TITLE: "The Odyssey",
+            AUTHOR: "Homer",
+            SUBJECT: "Epic Poetry",
+            "PUBLISH DATE": "800-01-01"
+          },
+          {
+            SNO: 26,
+            TITLE: "The Handmaid's Tale",
+            AUTHOR: "Margaret Atwood",
+            SUBJECT: "Dystopian",
+            "PUBLISH DATE": "1985-01-01"
+          },
+          {
+            SNO: 27,
+            TITLE: "A Tale of Two Cities",
+            AUTHOR: "Charles Dickens",
+            SUBJECT: "Historical Fiction",
+            "PUBLISH DATE": "1859-01-01"
+          },
+          {
+            SNO: 28,
+            TITLE: "The Great Gatsby",
+            AUTHOR: "F. Scott Fitzgerald",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1925-04-10"
+          },
+          {
+            SNO: 29,
+            TITLE: "The Secret Life of Bees",
+            AUTHOR: "Sue Monk Kidd",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "2001-11-08"
+          },
+          {
+            SNO: 30,
+            TITLE: "The Adventures of Huckleberry Finn",
+            AUTHOR: "Mark Twain",
+            SUBJECT: "Adventure",
+            "PUBLISH DATE": "1884-12-10"
+          },
+          {
+            SNO: 31,
+            TITLE: "The Stand",
+            AUTHOR: "Stephen King",
+            SUBJECT: "Horror",
+            "PUBLISH DATE": "1978-09-01"
+          },
+          {
+            SNO: 32,
+            TITLE: "The Canterbury Tales",
+            AUTHOR: "Geoffrey Chaucer",
+            SUBJECT: "Poetry",
+            "PUBLISH DATE": "1400-01-01"
+          },
+          {
+            SNO: 33,
+            TITLE: "Frankenstein",
+            AUTHOR: "Mary Shelley",
+            SUBJECT: "Gothic Fiction",
+            "PUBLISH DATE": "1818-01-01"
+          },
+          {
+            SNO: 34,
+            TITLE: "The Divine Comedy",
+            AUTHOR: "Dante Alighieri",
+            SUBJECT: "Epic Poetry",
+            "PUBLISH DATE": "1320-01-01"
+          },
+          {
+            SNO: 35,
+            TITLE: "Crime and Punishment",
+            AUTHOR: "Fyodor Dostoevsky",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1866-01-01"
+          },
+          {
+            SNO: 36,
+            TITLE: "The Scarlet Letter",
+            AUTHOR: "Nathaniel Hawthorne",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1850-01-01"
+          },
+          {
+            SNO: 37,
+            TITLE: "Jane Eyre",
+            AUTHOR: "Charlotte Brontë",
+            SUBJECT: "Gothic Fiction",
+            "PUBLISH DATE": "1847-01-01"
+          },
+          {
+            SNO: 38,
+            TITLE: "The Iliad",
+            AUTHOR: "Homer",
+            SUBJECT: "Epic Poetry",
+            "PUBLISH DATE": "760-01-01"
+          },
+          {
+            SNO: 39,
+            TITLE: "A Clockwork Orange",
+            AUTHOR: "Anthony Burgess",
+            SUBJECT: "Dystopian",
+            "PUBLISH DATE": "1962-01-01"
+          },
+          {
+            SNO: 40,
+            TITLE: "Lord of the Flies",
+            AUTHOR: "William Golding",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1954-09-17"
+          },
+          {
+            SNO: 41,
+            TITLE: "The Stranger",
+            AUTHOR: "Albert Camus",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1942-01-01"
+          },
+          {
+            SNO: 42,
+            TITLE: "The Grapes of Wrath",
+            AUTHOR: "John Steinbeck",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1939-04-14"
+          },
+          {
+            SNO: 43,
+            TITLE: "The Metamorphosis",
+            AUTHOR: "Franz Kafka",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1915-01-01"
+          },
+          {
+            SNO: 44,
+            TITLE: "The Adventures of Tom Sawyer",
+            AUTHOR: "Mark Twain",
+            SUBJECT: "Adventure",
+            "PUBLISH DATE": "1876-01-01"
+          },
+          {
+            SNO: 45,
+            TITLE: "The Handmaid's Tale",
+            AUTHOR: "Margaret Atwood",
+            SUBJECT: "Dystopian",
+            "PUBLISH DATE": "1985-01-01"
+          },
+          {
+            SNO: 46,
+            TITLE: "The Stranger",
+            AUTHOR: "Albert Camus",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1942-01-01"
+          },
+          {
+            SNO: 47,
+            TITLE: "The Grapes of Wrath",
+            AUTHOR: "John Steinbeck",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1939-04-14"
+          },
+          {
+            SNO: 48,
+            TITLE: "The Metamorphosis",
+            AUTHOR: "Franz Kafka",
+            SUBJECT: "Fiction",
+            "PUBLISH DATE": "1915-01-01"
+          },
+          {
+            SNO: 49,
+            TITLE: "The Adventures of Tom Sawyer",
+            AUTHOR: "Mark Twain",
+            SUBJECT: "Adventure",
+            "PUBLISH DATE": "1876-01-01"
+          },
+          {
+            SNO: 50,
+            TITLE: "The Book of Eli",
+            AUTHOR: "Gary Whitta",
+            SUBJECT: "Post-apocalyptic",
+            "PUBLISH DATE": "2010-01-15"
+          }
+    ]
+};
+//data.data.sort((a, b) => a.TITLE.localeCompare(b.TITLE));     Sorting data based on Title (localCompare compares two strings and declare Negative if a<b and Positive if a>b and Zero if a==0) 
+var table = document.getElementById("mytable");
+var tbody = table.getElementsByTagName("tbody")[0];
+
+for (var i = 0; i < 10; i++) {
+    var row = tbody.insertRow(i);
+    row.insertCell(0).innerHTML = i+1;
+    row.insertCell(1).innerHTML = data.data[i].TITLE;
+    row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+    row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+    row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+}
+const dropdownMenu = document.querySelector('.dropdown-menu');
+var count=0;
+let flatpickrInstance;
+// add an event listener to the dropdown menu
+dropdownMenu.addEventListener('click', function(event) {
+  // get the clicked dropdown item
+  const clickedItem = event.target;
+
+  // get the text content of the clicked dropdown item
+  const selectedItem = clickedItem.textContent.trim();
+ if(selectedItem==="Title")
+ {
+  document.getElementById("searchInput").value="";
+  count=1;
+  document.getElementById("bt").innerHTML="Title";
+  if (flatpickrInstance) {
+    flatpickrInstance.destroy();
+  }
+ }
+ else if(selectedItem==="Author")
+ {
+  document.getElementById("searchInput").value="";
+  count=2;
+  document.getElementById("bt").innerHTML="Author";
+    flatpickrInstance.destroy();
+  
+ }
+ else if(selectedItem==="Subject")
+ {
+  document.getElementById("searchInput").value="";
+  count=3;
+  document.getElementById("bt").innerHTML="Subject";
+  if (flatpickrInstance) {
+    flatpickrInstance.destroy();
+  }
+ }
+ else if(selectedItem==="Publish Date")
+ {
+  document.getElementById("searchInput").value="";
+  count=4;
+  document.getElementById("bt").innerHTML="Publish Date";
+  flatpickrInstance=flatpickr("#searchInput", {
+    dateFormat: "Y-m-d",
+    maxDate: "today"
+  });
+ } 
+console.log(count);
+});
+
+function searchBooks() {
+    var input = document.getElementById("searchInput").value;
+    var found = false;
+    var c=1;
+    tbody.innerHTML = ''; // clear existing table rows
+   if(count==1){
+    for (var i = 0; i < data.data.length; i++) {
+      if (data.data[i].TITLE.toLowerCase().includes(input.toLowerCase())) {
+        found = true;
+        var row = tbody.insertRow();
+        row.insertCell(0).innerHTML = c;
+        row.insertCell(1).innerHTML = data.data[i].TITLE;
+        row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+        row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+        row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+        c++;
+      }
+    }
+    if (!found) {
+      alert("Not available");
+    }
+  }
+  else if(count==2){
+    for (var i = 0; i < data.data.length; i++) {
+      if (data.data[i].AUTHOR.toLowerCase().includes(input.toLowerCase())) {
+        found = true;
+        var row = tbody.insertRow();
+        row.insertCell(0).innerHTML = c;
+        row.insertCell(1).innerHTML = data.data[i].TITLE;
+        row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+        row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+        row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+        c++;
+      }
+    }
+    if (!found) {
+      alert("Not available");
+    }
+  }
+  else if(count==3){
+    for (var i = 0; i < data.data.length; i++) {
+      if (data.data[i].SUBJECT.toLowerCase().includes(input.toLowerCase())) {
+        found = true;
+        var row = tbody.insertRow();
+        row.insertCell(0).innerHTML = c;
+        row.insertCell(1).innerHTML = data.data[i].TITLE;
+        row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+        row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+        row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+        c++;
+      }
+    }
+    if (!found) {
+      alert("Not available");
+    }
+  }
+  else if(count==4){
+    for (var i = 0; i < data.data.length; i++) {
+      if (data.data[i]["PUBLISH DATE"].toLowerCase().includes(input.toLowerCase())) {
+        found = true;
+        var row = tbody.insertRow();
+        row.insertCell(0).innerHTML = c;
+        row.insertCell(1).innerHTML = data.data[i].TITLE;
+        row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+        row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+        row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+        c++;
+      }
+    }
+    if (!found) {
+      alert("Not available");
+    }
+  }
+  }
+function changeActivePage(pageNum) {
+    // Get all the page links
+    var pageLinks = document.querySelectorAll('.pagination .page-link');
+    // Loop through the page links
+    for (var i = 0; i < pageLinks.length; i++) {
+      // Remove the "active" class from all links
+      pageLinks[i].parentNode.classList.remove('active');
+  
+      // If this is the link for the clicked page, add the "active" class
+      if (parseInt(pageLinks[i].innerHTML) === pageNum) {
+        pageLinks[i].parentNode.classList.add('active');
+      }
+    }
+    if (pageNum === 1) {
+      document.getElementById("searchInput").value="";
+        var table = document.getElementById("mytable");
+        var tbody = table.getElementsByTagName("tbody")[0];
+        tbody.innerHTML = ""; // clear the table body
+        
+        for (var i = 0; i < 10; i++) {
+          var row = tbody.insertRow(i); // adjust for index starting at 0
+          row.insertCell(0).innerHTML = i+1;
+          row.insertCell(1).innerHTML = data.data[i].TITLE;
+          row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+          row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+          row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+        }
+      }
+    else if (pageNum === 2) {
+      document.getElementById("searchInput").value="";
+      var table = document.getElementById("mytable");
+      var tbody = table.getElementsByTagName("tbody")[0];
+      tbody.innerHTML = ""; // clear the table body
+      
+      for (var i = 10; i < 20; i++) {
+        var row = tbody.insertRow(i-10); // adjust for index starting at 0
+        row.insertCell(0).innerHTML = i+1;
+        row.insertCell(1).innerHTML = data.data[i].TITLE;
+        row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+        row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+        row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+      }
+    } else if (pageNum === 3) {
+      document.getElementById("searchInput").value="";
+      var table = document.getElementById("mytable");
+      var tbody = table.getElementsByTagName("tbody")[0];
+      tbody.innerHTML = ""; // clear the table body
+      
+      for (var i = 20; i < 30; i++) {
+        var row = tbody.insertRow(i-20); // adjust for index starting at 0
+        row.insertCell(0).innerHTML = i+1;
+        row.insertCell(1).innerHTML = data.data[i].TITLE;
+        row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+        row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+        row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+      }
+    } else if (pageNum === 4) {
+        document.getElementById("searchInput").value="";
+        var table = document.getElementById("mytable");
+        var tbody = table.getElementsByTagName("tbody")[0];
+        tbody.innerHTML = ""; // clear the table body
+        
+        for (var i = 30; i < 40; i++) {
+          var row = tbody.insertRow(i-30); // adjust for index starting at 0
+          row.insertCell(0).innerHTML = i+1;
+          row.insertCell(1).innerHTML = data.data[i].TITLE;
+          row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+          row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+          row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+        }
+      } else if (pageNum === 5) {
+        document.getElementById("searchInput").value="";
+        var table = document.getElementById("mytable");
+        var tbody = table.getElementsByTagName("tbody")[0];
+        tbody.innerHTML = ""; // clear the table body
+        
+        for (var i = 40; i < 50; i++) {
+          var row = tbody.insertRow(i-40); // adjust for index starting at 0
+          row.insertCell(0).innerHTML = i+1;
+          row.insertCell(1).innerHTML = data.data[i].TITLE;
+          row.insertCell(2).innerHTML = data.data[i].AUTHOR;
+          row.insertCell(3).innerHTML = data.data[i].SUBJECT;
+          row.insertCell(4).innerHTML = data.data[i]["PUBLISH DATE"];
+        }
+      }
+    
+  }
+  
